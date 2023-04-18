6a87e74ffef6e48f9945312b367ad80248dd7558
keithtjj <keithtjj@gmail.com>
2023-04-18 16:03:46 +0800

    fix for update condition

--- navi.py
+++ navi.py
@@ -33,7 +33,7 @@ def save_poi(msg):
 
 def update_goal_status(msg):
     global n, engage, poi_wp_list
-    if msg.data == True:    
+    if msg.data == True and len(poi_wp_list) != 0:    
         engage = True
         pub_arrival.publish(String(poi_wp_list[0].header.frame_id))
         poi_wp_list.remove(poi_wp_list[0])
