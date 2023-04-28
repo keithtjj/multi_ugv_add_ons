c8b93c6f68d7f716cac1525189c8d5fd4b7c977f
keithtjj <keithtjj@gmail.com>
2023-04-28 15:59:40 +0800

    rm cv from hunter and killer, fully using yolo

--- hunter.py
+++ hunter.py
@@ -1,60 +1,23 @@
 #!/usr/bin/env python
 
 import rospy 
-from sensor_msgs.msg import Image 
 from std_msgs.msg import Header
-from geometry_msgs.msg import PointStamped, PoseStamped, Pose
+from geometry_msgs.msg import PoseStamped, Pose
 from nav_msgs.msg import Odometry
-from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
-import cv2 
-import numpy as np
 from yolov7_ros.msg import Detection, Detections
 from random import randint
 
 pub_poi = rospy.Publisher('/poi_out', PoseStamped, queue_size=10)
 
-bridge = CvBridge()
 engage = False
 current_pose = Pose()
 poi_list = []
 yolo_dets = []
 
-def callback(data):
-    raw = bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
-    pub_poi.publish(PoseStamped(header=Header(stamp=rospy.Time.now(),frame_id='test'), pose=current_pose))
-
-    #find doors
-    lower_b = np.array([0,100,100])
-    upper_b = np.array([0,130,130])
-    mask = cv2.inRange(raw, lower_b, upper_b)
-    cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for c in cnts[0]:
-        cv2.drawContours(raw, [c], -1, (0, 255, 0), 2)
-        area = cv2.contourArea(c)
-        #rospy.loginfo(area)
-        if area > 8000 and compare_pose(4.5):
-            pub_poi.publish(PoseStamped(header=Header(stamp=rospy.Time.now(),frame_id='door'), pose=current_pose))
-            poi_list.append(current_pose)
-            rospy.loginfo('door')
-
-    # detect people in the image
-    for d in yolo_dets:
-        b = d.bbox
-        colour=(randint(0,255), randint(0,255), randint(0,255))
-        cv2.rectangle(raw, (b[0], b[1]), (b[2], b[3]), colour, 1)
-        center = int((b[0]+b[2])/2), int((b[1]+b[3])/2)
-        
-    raw = cv2.resize(raw, (0,0), fx=2,fy=2)
-    cv2.imshow("hunter", raw)
-    cv2.waitKey(1)
-
 def process_detects(detects):
-    global poi_list, yolo_dets
-    yolo_dets = []
+    global poi_list
     for d in detects.dets:
         yolo_dets.append(d)
-        if d.name != 'person':
-            continue
         b = d.bbox
         center = int((b[0]+b[2])/2), int((b[1]+b[3])/2)
         if compare_pose(5):
@@ -81,8 +44,6 @@ def get_pos(data):
   
 if __name__ == '__main__':
     rospy.init_node('hunter')
-    rospy.Subscriber('/camera/image', Image, callback, queue_size=1, buff_size=2**24)
     rospy.Subscriber('/state_estimation', Odometry, get_pos)
     rospy.Subscriber('/detections', Detections, process_detects)    
     rospy.spin()
-    cv2.destroyAllWindows()
--- killer.py
+++ killer.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python
 
 import rospy
-from sensor_msgs.msg import Image 
 from std_msgs.msg import Header, Bool, String
 from geometry_msgs.msg import TwistStamped, Twist, Vector3
 from nav_msgs.msg import Odometry
-from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
-import cv2 
-import numpy as np
 from yolov7_ros.msg import Detection, Detections
-from random import randint
 
 pub_vel = rospy.Publisher('/cmd_vel', TwistStamped, queue_size=5)
 pub_engaged = rospy.Publisher('/engaged', Bool, queue_size=5)
 pub_kill = rospy.Publisher('/del_model_out', String, queue_size=5)
 
-bridge = CvBridge()
 door_list = [['door1', 43, -9], ['door2', 85, -4], ['door4', 56, 57], ['door3', 10, -2]]
 engage = False
-yolo_dets = []
-persons = []
+arrival = False
+rawX = 320
+targets = []
 
 def get_pos(data):
     global current_pose 
@@ -37,86 +32,38 @@ def del_model_sel(m):
         if dxy < d:
             d = dxy
             m = mo[0]
-    print('del' + m)
+    print('del ' + m)
     pub_kill.publish(String(m))
 
-def detector(data):
-    global engage, arrival
-    raw = bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
-    pub_kill.publish(String('test'))
-    
-    #find doors
-    #(T, thresh) = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY_INV)
-    lower_b = np.array([0,100,100])
-    upper_b = np.array([0,130,130])
-    mask = cv2.inRange(raw, lower_b, upper_b)
-    cnts, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for c in cnts:
-        cv2.drawContours(raw, [c], -1, (0, 255, 0), 2)
-
-    # detect people in the image
-    # returns the bounding boxes for the detected objects
-    for d in yolo_dets:
-        colour=(randint(0,255), randint(0,255), randint(0,255))
-        b = d.bbox
-        cv2.rectangle(raw, (b[0], b[1]), (b[2], b[3]), colour, 1)
-        center = int((b[0]+b[2])/2), int((b[1]+b[3])/2)
-    raw = cv2.resize(raw, (0,0), fx=2,fy=2)
-
-    cv2.imshow("killer", raw)
-    cv2.waitKey(1)
-    if not arrival:
-        return
-    
+def process_detects(detects):
+    global targets
+    targets = []
+    for d in detects.dets:
+        if d.name == poi_type:
+            targets.append(d.bbox)
     lin_vel = 0
-    ang_vel = 0
-    _, raw_x, _ = raw.shape
-    if poi_type == 'door':
-        for c in cnts:
-            M = cv2.moments(c)
-            centerX = int(M["m10"] / (M["m00"]+1))
-            if raw_x/3 < centerX < raw_x*2/3 and M["m00"] > 20000:
-                del_model_sel('door')
-                pub_engaged.publish(Bool(False))
-                rospy.loginfo('destroyed')
-                arrival = False
-            else:
-                ang_vel = 10 * (1-2*centerX/raw_x)
-        if len(cnts)==0:
-            ang_vel = 10
-
-    elif poi_type == 'person':
-        if len(persons) == 1:
-            for (x1, y1, x2, y2) in persons:
-                center = int((x1+x2)/2), int((y1+y2)/2)
-                rospy.loginfo('identified')
-                if raw.shape[1]/3 < centerX < raw.shape[1]*2/3:
-                    rospy.loginfo('engage')
-                    pub_kill.publish(String('person'))
+    ang_vel = 10
+    for (x1, y1, x2, y2) in targets:
+        if len(targets) == 1:
+            centerX = int((x1+x2)/2)
+            rospy.loginfo('identified')
+            if poi_type == 'door':
+                if rawX/3 < centerX < rawX*2/3:
+                    del_model_sel('door')
                     pub_engaged.publish(Bool(False))
-                    rospy.loginfo('unalived')
                     arrival = False
                 else:
-                    rospy.loginfo('aiming')
-                    ang_vel = 10 * (1-2*center[0]/raw_x)
-                    h = y2-y1
-                    if h < 90:
-                        lin_vel = 10 * (1-h/90)
+                    ang_vel = 10 * (1-2*centerX/rawX)
+            elif poi_type == 'person':
+                h = y2-y1
+                ang_vel = 10 * (1-2*centerX/rawX)
+                lin_vel = 10 * (1-h/90)
+            else:
+                rospy.loginfo('idk what to do')
         else:
-            ang_vel = 10
-            rospy.loginfo('not found')
-    
+            rospy.loginfo('too many/none')
     pub_vel.publish(TwistStamped(header=Header(stamp=rospy.Time.now(),frame_id='vehicle'),
                                 twist=(Twist(linear = Vector3(lin_vel,0,0), angular = Vector3(0,0,ang_vel)))))
-
-def process_detects(detects):
-    global yolo_dets, persons
-    yolo_dets = []
-    persons = []
-    for d in detects.dets:
-        yolo_dets.append(d)
-        if d.name == 'person':
-                persons.append(d.bbox)
 
 def arrived(target):
     global arrival, poi_type
@@ -124,11 +71,8 @@ def arrived(target):
     poi_type = target.data
 
 if __name__ == '__main__':
-    global arrival
-    arrival = False
     rospy.init_node('killer')
     rospy.Subscriber('/arrival', String, arrived)
-    rospy.Subscriber('/camera/image', Image, detector, queue_size=1, buff_size=2**24)
     rospy.Subscriber('/state_estimation', Odometry, get_pos)
     rospy.Subscriber('/detections', Detections, process_detects)
     rospy.spin()
